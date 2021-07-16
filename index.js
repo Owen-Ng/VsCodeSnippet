a2acc174d43a952b0df709f67f380bb34d763189
Owen Ng <nigel.ngyanhing@mail.utoronto.ca>
2021-07-16 02:03:47 -0400

    update

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // })
     inputField.addEventListener('input', function(event){
         const snippet = toSnippet(event.target.value);
-        outputField.value = snippet.replace(new RegExp(",", "g"), ",\n");
+        outputField.value = snippet.replace(new RegExp('","', "g"), '",\n"');
         //That g on RegExp means globally so the regex will be apply everywhere in the snippet
     })
 });
